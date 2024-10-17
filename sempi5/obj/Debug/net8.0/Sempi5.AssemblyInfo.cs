f28d600e4338d836fd48a7d4a978cd7e300605fd
Mateus Maria Gomes Eça de Queiroz Cabral <1220704@isep.ipp.pt>
2024-10-17 17:16:13 +0100

    Correction of some small mistakes

--- sempi5/obj/Debug/net8.0/Sempi5.AssemblyInfo.cs
+++ sempi5/obj/Debug/net8.0/Sempi5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sempi5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7f5d08eeda1c36b207ae1b236fe0e4abc5671d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31bebb004fc5082e3abcab8565d9a776df149a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sempi5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sempi5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
