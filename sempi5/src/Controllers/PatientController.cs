356e699337d2d2642214a3f4b19623a23fa7ad58
sdr1221121 <1221121@isep.ipp.pt>
2024-10-20 23:44:46 +0100

    #3 implement httpGet method to confirm email and save patien on repository

--- sempi5/src/Controllers/PatientController.cs
+++ sempi5/src/Controllers/PatientController.cs
@@ -1,19 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sempi5.Services;
 
 namespace Sempi5.Controllers;
 
 [ApiController]
-[Route("api/email")]
+[Route("api/[controller]")]
 public class PatientController : ControllerBase
 {
-    // Endpoint to track email click
-    [HttpGet("track-email-click")]
-    public IActionResult TrackEmailClick(string email, string token)
+    private readonly PatientService patientService;
+
+    public PatientController()
+    {
+        patientService = new PatientService();
+    }
+
+    
+    [HttpGet("email/track-email-click")]
+    public async Task<IActionResult> TrackEmailClick(string email, string token)
     {
         // Register that the link was clicked
+
         Console.WriteLine($"O link foi acessado pelo email: {email} com o token: {token} em {DateTime.UtcNow}");
-        
-        // Redirect to the confirmation link
+        // Confirm the email
+        await patientService.confirmEmail(email, token);
+
+
         return Ok($"Acesso registrado para o email: {email}. Obrigado por confirmar.");
     }
+    
 }
---
+++ sempi5/src/Services/PatientService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Sempi5.Domain.Patient;
+using Sempi5.Domain.Shared;
+using Sempi5.Infrastructure.Databases;
+using Sempi5.Infrastructure.PatientRepository;
+
+namespace Sempi5.Services;
+
+public class PatientService
+{
+    private readonly DBContext context;
+    private readonly IPatientRepository _patientRepository;
+
+    public PatientService()
+    {
+        _patientRepository = new PatientRepository(context);
+    }
+
+    public async Task confirmEmail(string email, string token)
+    {
+        var patient = await _patientRepository.GetByEmail(email);
+
+        if (patient == null)
+        {
+            throw new Exception("Paciente não encontrado");
+        }
+
+        if (patient.User.IsVerified)
+        {
+            throw new Exception("Email já confirmado");
+        }
+
+
+        patient.User.IsVerified = true;
+
+        await _patientRepository.AddAsync(patient);
+    }
+
+}
