f528f899b839b5e7bc670955cfb289ecbe522997
Rui <1221283@isep.ipp.pt>
2024-10-22 11:55:30 +0100

    #11 Patient Repository methods

--- sempi5/src/Infrastructure/PatientRepository/IPatientRepository.cs
+++ sempi5/src/Infrastructure/PatientRepository/IPatientRepository.cs
@@ -4,8 +4,14 @@ using Sempi5.Domain.User;
 
 namespace Sempi5.Infrastructure.PatientRepository
 {
-    public interface IPatientRepository : IRepository<Patient,PatientId>
+    public interface IPatientRepository : IRepository<Patient,MedicalRecordNumber>
     {
         public Task<Patient> GetByEmail(string email);
+        
+        public Task<Patient> GetByName(string name);
+        
+        public Task<Patient> GetByDateOfBirth(DateTime dateOfBirth);
+        
+        public Task<Patient> GetByMedicalRecordNumber(string medicalRecordNumber);
     }
 }
