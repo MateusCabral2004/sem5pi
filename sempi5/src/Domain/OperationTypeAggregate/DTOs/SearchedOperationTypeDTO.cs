454052d914a8cd66588a0463b11e8b03bcde5fb7
rute <1220994@isep.ipp.pt>
2024-10-26 23:18:30 +0100

    #23 correcting repository

---
+++ sempi5/src/Domain/OperationTypeAggregate/DTOs/SearchedOperationTypeDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sempi5.Domain.OperationTypeAggregate.DTOs;
+
+public class SearchedOperationTypeDTO
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public string requiredStaff { get; set; }
+    public string estimatedDuration { get; set; }
+}
