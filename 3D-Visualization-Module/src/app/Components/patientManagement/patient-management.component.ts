a553952fffa7a2e17f6baf67246c3ad610243844
rute <1220994@isep.ipp.pt>
2024-11-07 16:22:24 +0000

    #35 creating menu for the patient management

---
+++ 3D-Visualization-Module/src/app/Components/patientManagement/patient-management.component.ts
@@ -0,0 +1,31 @@
+﻿import {Component, Inject} from '@angular/core';
+import {AuthService} from '../../services/AuthService/auth.service';
+import {Router} from '@angular/router';
+
+
+@Component({
+  selector: 'app-patient-management',
+  templateUrl: './patient-management.component.html',
+  styleUrls: ['./patient-management.component.css']
+})
+
+export class PatientManagementComponent{
+  private auth: AuthService;
+
+
+  menuItems=[
+    {title:'Create Patient Profile', icon:'assets/icon/dashboard.png', link:'/add'},
+    {title:'Update Patient Profile', icon:'assets/icon/dashboard.png', link:'/update'},
+    {title:'Delete Patient Profile', icon:'assets/icon/dashboard.png', link:'/delete'}
+  ];
+
+  constructor(@Inject(AuthService) auth: AuthService, private router:Router) {
+    this.auth=auth;
+    this.validateUserRole();
+  }
+
+  private validateUserRole(){
+    const expectedRole='Admin';
+    this.auth.validateUserRole(expectedRole);
+  }
+}
