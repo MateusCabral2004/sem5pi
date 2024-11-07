eaa5461ee247f658ae8e36056b6c5dbc660a7ea7
rute <1220994@isep.ipp.pt>
2024-11-07 18:44:53 +0000

    #43 implementing front end for the operation request

---
+++ 3D-Visualization-Module/src/app/Components/add-operation-request/add-operation-request.component.ts
@@ -0,0 +1,9 @@
+﻿
+// @Component({
+//   selector:'app-add-op-request',
+//   templateUrl:'./add-operation-request.component.html',
+//   styleUrls: ['./add-operation-request.component.css']
+// })
+//
+// export class AddOperationRequestComponent{
+// }
