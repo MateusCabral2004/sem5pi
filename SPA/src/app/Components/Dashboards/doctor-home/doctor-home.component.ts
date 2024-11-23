86317c274b0cba21c79235687b03381bb7a39657
Mateus Maria Gomes Eça de Queiroz Cabral <1220704@isep.ipp.pt>
2024-11-23 21:47:28 +0000

    Small update to the dashboards

--- SPA/src/app/Components/Dashboards/doctor-home/doctor-home.component.ts
+++ SPA/src/app/Components/Dashboards/doctor-home/doctor-home.component.ts
@@ -14,7 +14,7 @@ export class DoctorMenuComponent {
     /* The menus are temporary and will need to be replaced with the actual menu items
        but because they are not yet implemented I created some placeholders */
     { title: 'Operation Request Management', icon: 'assets/icons/operationRequest.png', link: '/doctor/operationRequests' },
-
+    { title: '3D',icon: 'assets/icons/3d.png', link: '/3d' }
   ];
 
   constructor(@Inject(AuthService) auth: AuthService) {
